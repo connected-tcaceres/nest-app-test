403ca669e58efd1f3d512a36ff8575cb66cb7d71
Tyler Caceres <tcaceres@connected.io>
2022-01-05 14:27:17 -0500

    add student controller

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { StudentController } from '../student/student.controller';
 
 @Module({
   imports: [],
-  controllers: [],
+  controllers: [StudentController],
   providers: [],
 })
 export class AppModule {}
